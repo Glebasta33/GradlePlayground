591f35293fc6a728074a2b44f3ba0f40477dbe4a
Трусов Глеб Леонидович <GLeTrusov@sberbank.ru>
2023-07-09 10:25:27 +0300

    Add testlib module

--- app/src/main/java/com/example/gradleplayground/MainActivity.kt
+++ app/src/main/java/com/example/gradleplayground/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.gradleplayground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.testlib.TestValKeeper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val valueFromAnotherModule = TestValKeeper.value
+        Toast.makeText(this, valueFromAnotherModule, Toast.LENGTH_SHORT).show()
     }
 }
---
+++ testlib/src/main/java/com/example/testlib/TestValKeeper.kt
@@ -0,0 +1,5 @@
+package com.example.testlib
+
+object TestValKeeper {
+    val value = "Value from library"
+}
